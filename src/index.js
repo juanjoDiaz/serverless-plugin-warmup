d20c078e4cf02abf820eb816c73693eb3f2797c6
Juanjo Diaz <juanjo.diaz@intopalo.com>
2019-01-26 12:42:01 +0200

    Fix payload being stringified twice

--- src/index.js
+++ src/index.js
@@ -174,7 +174,7 @@ class WarmUP {
 
     const functionDefaultOpts = {
       enabled: false,
-      source: JSON.stringify({ source: 'serverless-plugin-warmup' }),
+      source: { source: 'serverless-plugin-warmup' },
       concurrency: 1
     }
 
