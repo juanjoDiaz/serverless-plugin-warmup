516a14d3a0a7786a237ccf7d9fbca5cf1654ef33
Juanjo Diaz <juanjo.diaz@intopalo.com>
2019-01-27 22:33:58 +0200

    Add tests for after:deploy:deploy hook

---
+++ test/utils/configUtils.js
@@ -0,0 +1,33 @@
+function getServerlessConfig (serverless = {}) {
+  return {
+    getProvider: serverless.getProvider || (() => {}),
+    config: {
+      servicePath: (serverless.config && serverless.config.servicePath) ? serverless.config.servicePath : 'testPath'
+    },
+    cli: {
+      log() {}
+    },
+    service: {
+      provider: (serverless.service && serverless.service.provider)
+        ? serverless.service.provider
+        : { stage: '', region: '' },
+      defaults: (serverless.service && serverless.service.defaults)
+        ? serverless.service.defaults
+        : { stage: '', region: '' },
+      service: 'warmup-test',
+      custom: (serverless.service && serverless.service.custom) ? serverless.service.custom : {},
+      getAllFunctions() { return Object.keys(this.functions) },
+      getFunction(name) { return this.functions[name] },
+      functions: (serverless.service && serverless.service.functions) ? serverless.service.functions : {}
+    }
+  }
+}
+
+function getOptions (options = {}) {
+  return options
+}
+
+module.exports = {
+  getServerlessConfig,
+  getOptions
+}
