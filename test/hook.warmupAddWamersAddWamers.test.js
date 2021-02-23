3714b0b84561883d9a6142a06b9522813c2c4f9e
Juanjo Diaz <juanjo.diazmo@gmail.com>
2021-02-23 19:26:04 +0100

    refactor: revert "replace `after:package:initialize` by `'before:package:createDeploymentArtifacts`"
    This reverts commit c46f7b8a97a407a1cc6ec2138bcf471e9a8daaf0.

--- test/hook.warmupAddWamersAddWamers.test.js
+++ test/hook.warmupAddWamersAddWamers.test.js
@@ -50,7 +50,7 @@ describe('Serverless warmup plugin warmup:warmers:addWamers:addWamers hook', () 
     });
     const plugin = new WarmUp(serverless, {});
 
-    await plugin.hooks['before:package:createDeploymentArtifacts']();
+    await plugin.hooks['after:package:initialize']();
 
     expect(serverless.pluginManager.spawn).toHaveBeenCalledTimes(1);
     expect(serverless.pluginManager.spawn).toHaveBeenCalledWith('warmup:addWamers');
