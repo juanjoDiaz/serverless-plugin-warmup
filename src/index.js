a71427fb28766117766c0f154395811facc87a86
Juanjo Diaz <juanjo.diazmo@gmail.com>
2021-02-23 20:18:57 +0100

    feat: add workaround for webpack/bundle plugin

--- src/index.js
+++ src/index.js
@@ -68,6 +68,8 @@ class WarmUp {
       'warmup:cleanupTempDir:cleanup': this.cleanUp.bind(this),
       'before:warmup:prewarm:start': this.configPlugin.bind(this),
       'warmup:prewarm:start': this.prewarmFunctions.bind(this),
+      // Workaround webpack/bundle plugins, reset the plugin and ignore changes
+      'before:package:createDeploymentArtifacts': this.initializeWarmers.bind(this),
     };
   }
 
