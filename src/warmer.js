1c5a4911df52b3d4667265e137ebc4a2c25d364f
Juanjo Diaz <juanjo.diazmo@gmail.com>
2022-02-09 18:12:18 +0100

    feat: update functions ARNs in lambda role to adjust to new AWs rules

--- src/warmer.js
+++ src/warmer.js
@@ -94,7 +94,7 @@ function addWarmUpFunctionRoleToResources(service, stage, warmerName, warmerConf
                   'lambda:InvokeFunction',
                 ],
                 Resource: warmerConfig.functions.map((fn) => ({
-                  'Fn::Sub': `arn:\${AWS::Partition}:lambda:\${AWS::Region}:\${AWS::AccountId}:function:${fn.name}`,
+                  'Fn::Sub': `arn:\${AWS::Partition}:lambda:\${AWS::Region}:\${AWS::AccountId}:function:${fn.name}*`,
                 })),
               },
               {
